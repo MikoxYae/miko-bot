c37a5edc0d8e9bb944b27414d3338364947b8eaa
Yae Miko <aloksinghkumar756@gmail.com>
2026-05-03 18:26:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ OWNER = os.environ.get("OWNER", "Anythingbutnew56")
 OWNER_ID = int(os.environ.get("OWNER_ID", "8229041976"))
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Payal:Aloksingh@payal.jv2kwch.mongodb.net/?appName=Payal")
 DB_NAME = os.environ.get("DATABASE_NAME", "Angle")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8717980062:AAGf3y0VFNtxUE0vQdYVkgI4CQrBPW6_zAU")
 
 _start_pics_env = os.environ.get("START_PIC", "")
 START_PICS = [p.strip() for p in _start_pics_env.split() if p.strip()] or [
