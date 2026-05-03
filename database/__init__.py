33df7b9cb59fb9d5073f9e3726b3979396699246
Yae Miko <aloksinghkumar756@gmail.com>
2026-05-03 18:38:25 +0530

    fix: export get_next_pic_index from database __init__

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,1 @@
-from .db import add_user, is_user_exist, get_all_users, total_users_count, delete_user
+from .db import add_user, is_user_exist, get_all_users, total_users_count, delete_user, get_next_pic_index
